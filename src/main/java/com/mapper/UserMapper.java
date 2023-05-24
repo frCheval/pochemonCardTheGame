70fd09dabfb380df65e24cc3131504d35661805d
FranCHEVAL <francois.cheval@cpe.fr>
2023-05-24 14:07:42 +0200

    correct-bug

--- src/main/java/com/mapper/UserMapper.java
+++ src/main/java/com/mapper/UserMapper.java
@@ -1,5 +1,13 @@
 package com.mapper;
 
-public interface UserMapper {
+import org.mapstruct.Mapper;
 
+import com.dto.UserDTO;
+import com.entity.User;
+
+
+@Mapper
+public interface UserMapper {
+	public UserDTO toUserDTO(User user);
+	public User toUser(UserDTO userDTO);
 }
