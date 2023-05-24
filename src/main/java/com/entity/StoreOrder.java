a8524e3f2327158aee73dd81ce3776c0bd9ff391
GAUCHOUX Xavier <xavier.gauchoux@soprasteria.com>
2023-05-24 11:29:03 +0200

    Updated Entity

--- src/main/java/com/entity/StoreOrder.java
+++ src/main/java/com/entity/StoreOrder.java
@@ -1,6 +1,9 @@
 package com.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +12,12 @@ import lombok.Setter;
 @Setter
 public class StoreOrder {
 	
+	@OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
 	Integer userId;
 	
+	@OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cardId")
 	Integer cardId;
 
 }
--- src/main/java/com/entity/StoreTransaction.java
+++ src/main/java/com/entity/StoreTransaction.java
@@ -5,8 +5,11 @@ import java.util.Date;
 import com.enums.Action;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,9 +22,14 @@ public class StoreTransaction {
 	@GeneratedValue
 	Integer id;
 	
+	@OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
 	Integer userId;
 	
+	@OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cardId")
 	Integer cardId;
+
 	
 	Action action;
 	
