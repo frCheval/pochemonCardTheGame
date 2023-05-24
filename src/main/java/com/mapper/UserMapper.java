871aac888de8e9656544b1cbbfffe12bc7e38ce3
Martin <martin.jourjon42@gmail.com>
2023-05-24 16:27:06 +0200

    Feat: Finished user service/controller

--- src/main/java/com/mapper/UserMapper.java
+++ src/main/java/com/mapper/UserMapper.java
@@ -5,8 +5,11 @@ import org.mapstruct.Mapper;
 import com.dto.UserDTO;
 import com.entity.User;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 	public UserDTO toUserDTO(User user);
 	public User toUser(UserDTO userDTO);
+	public List<UserDTO> toUserDTOList(List<User> userList);
 }
