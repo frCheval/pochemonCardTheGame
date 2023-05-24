4ad93366cee616813d68b7a7871d015599db71c5
GAUCHOUX Xavier <xavier.gauchoux@soprasteria.com>
2023-05-24 09:35:32 +0200

    Started Project

---
+++ src/main/java/com/tuto/springboot/SPWebAppStep1/SpWebAppStep1Application.java
@@ -0,0 +1,13 @@
+package com.tuto.springboot.SPWebAppStep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpWebAppStep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpWebAppStep1Application.class, args);
+	}
+
+}
