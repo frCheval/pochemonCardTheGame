8ea7c2fc2726b8149a08610940678146f73ed6dd
FranCHEVAL <francois.cheval@cpe.fr>
2023-05-26 17:39:41 +0200

    ADD ON CONFLICT TO

--- src/main/resources/data-import.sql
+++ src/main/resources/data-import.sql
@@ -1,7 +1,9 @@
-INSERT INTO users (login, pwd, account, last_name, first_name, email) VALUES
-('admin', 'admin', 1000000, 'admin', 'admin', 'admin@gmail.com'),
-('user', 'user', 1000, 'user', 'user', 'user@gmail.com'),
-('user2', 'user2', 1000, 'user2', 'user2', 'user2@gmail.com');
+INSERT INTO  users (id,login, pwd, account, last_name, first_name, email) VALUES
+(1,'admin', 'admin', 1000000, 'admin', 'admin', 'admin@gmail.com'),
+(2,'user', 'user', 1000, 'user', 'user', 'user@gmail.com'),
+(3,'user2', 'user2', 1000, 'user2', 'user2', 'user2@gmail.com')
+ON CONFLICT(id) DO NOTHING;
 
-INSERT INTO card (name, description, family, affinity, energy, hp, defence, attack, user_Id) VALUES
-('Pokecrotte', 'oui oui baguette', 'La miff', 'oui', 100, 100, 100, 100, 1)
+INSERT INTO card (id,name, description, family, affinity, energy, hp, defence, attack, user_Id) VALUES
+(1,'Pokecrotte', 'oui oui baguette', 'La miff', 'oui', 100, 100, 100, 100, 1)
+ON CONFLICT(id) DO NOTHING;
